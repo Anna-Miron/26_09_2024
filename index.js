8d0aea25f649dc859cb54ee32fc3a8f5fc11d152
Anna-Miron <ana04miron@gmail.com>
2024-09-26 16:43:28 +0100

    add variables

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("Hello World!");
+
+// variabila declarata si apoi atribuita valoare
+let userName;
+userName = "Monica";
+console.log(userName);
+userName = "_monica_";
+console.log(userName);
+
+// variabila declarata si initiata cu o valoare intr-o linie
+let email = "user@test.com";
+console.log(email);
+
+// constanta declarata cu  (nu se poate reatribui valoare contantei)
+const pi = 3.14;
